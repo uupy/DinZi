5faefb0e57ff359788300f1643a1b98b6e4fa537
TeemoPeng <danielpog@163.com>
2017-07-07 21:55:53 +0800

    router config

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,9 @@ import Router from 'vue-router'
 
 const Home = resolve => require(['@/views/home.vue'], resolve)
 const Index = resolve => require(['@/views/home/index.vue'], resolve)
+const Case = resolve => require(['@/views/case/case.vue'], resolve)
+const About = resolve => require(['@/views/about/about.vue'], resolve)
+const News = resolve => require(['@/views/news/news.vue'], resolve)
 
 Vue.use(Router)
 
@@ -16,8 +19,38 @@ export default new Router({
       	children: [{
         	path: '/index',
         	component: Index,
-        	name: '首页'
+        	name: ''
     	}]
+    },
+    {
+        path: '/case',
+        name:'案例',
+        component:Home,
+        children: [{
+          path: '/case/',
+          component: Case,
+          name: ''
+      }]
+    },
+    {
+        path: '/about',
+        name:'关于',
+        component:Home,
+        children: [{
+          path: '/about/',
+          component: About,
+          name: ''
+      }]
+    },
+    {
+        path: '/news',
+        name:'动态',
+        component:Home,
+        children: [{
+          path: '/news/',
+          component: News,
+          name: ''
+      }]
     }
   ]
 })
