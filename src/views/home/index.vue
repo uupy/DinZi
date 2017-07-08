62c7282995f068111566f40e92bf9189ff94b442
TeemoPeng <danielpog@163.com>
2017-07-08 11:32:52 +0800

    index completed

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -36,13 +36,57 @@
 							<img src='../../../static/images/img_company_01.jpg' />
 						</el-col>
 						<el-col :span="14" class='about-img'>
+							<div class='about-txt'>
+								<div class='para p1'>
+									<h3>服务宗旨</h3>
+									<p>用我的专业技术，让您的品牌价值再提升一步，</p>
+									<p>这是我的立业之本。</p>
+								</div>
+								<div class="para p2">
+									<h3>核心价值观</h3>
+									<p><span>专业高效</span><span>优质出品</span><span>至诚至信</span></p>
+								</div>
+								<div class="para p3">
+									<p>专注中小企业品牌设计服务</p>
+								</div>
+							</div>
 							<img src='../../../static/images/img_company_02.jpg' />
 						</el-col>
 					</el-col>
 					<el-col :span="22" :offset='1' class='team'>
-						<img src='../../../static/images/team.png' />
+						<p class="team-title">专业的品牌营销思维及设计团队</p>
+						<p class="team-desc">快速反应 \ 高效无缝对接客户</p>
 					</el-col>
-				</el-col>				
+					<el-col class='more'>					
+						<span class='more-btn' @click='joinDinz'>Join DiNZ</span>
+					</el-col>
+				</el-col>	
+
+				<el-col class='news'>
+					<el-col :span='22' :offset='1'>
+						<el-col :span='8' class='news-wrap'>
+							<ul>
+								<li v-for='item in newsList1'>
+									{{item.title}}
+								</li>
+							</ul>
+						</el-col>
+						<el-col :span='8' class='news-wrap'>
+							<ul>
+								<li v-for='item in newsList2'>
+									{{item.title}}
+								</li>
+							</ul>
+						</el-col>
+						<el-col :span='8' class='news-wrap'>
+							<ul>
+								<li v-for='item in newsList3'>
+									{{item.title}}
+								</li>
+							</ul>
+						</el-col>
+					</el-col>
+				</el-col>			
 			</el-col>
 		</el-col>
 	</el-row>
@@ -86,17 +130,68 @@
 
 
 	.about{margin-top:30px;}
-	.about-title{color:#151515;text-align: center;margin-bottom: 30px;font-size: 24px;}
-	.about-img{box-sizing: border-box;padding-left:5px;overflow: hidden}
+	.about-title{color:#151515;text-align: center;margin-bottom: 30px;font-size: 24px;padding:0 15px;}
+	.about-img{box-sizing: border-box;padding-left:5px;overflow: hidden;position: relative}
+	.about-img:first-child{padding-left: 0;}
 	.about-img img {width:100%;height: 420px;}
-	.team{margin-top: 5px;box-sizing: border-box;overflow: hidden;margin-bottom: 50px;}
+	.team{margin-top: 5px;box-sizing: border-box;overflow: hidden;text-align: center;background: #535353;color:#fff;padding: 12rem 0;}
 	.team img{padding-left: 5px;width:100%;max-height: 430px;}
+	.team-title{font-size: 36px;line-height: 2;}
+	.team-desc{color:#FDD900}
+
+	.about-txt{position: absolute;left:20px;top:20px;z-index: 999;color:#fff;}
+	.about-txt h3{font-weight: normal;padding: 15px 0;transform:translateX(-100%);-webkit-transform:translateX(-100%);-moz-transform:translateX(-100%);-ms-transform:translateX(-100%);transition:all .5s 0s;-webkit-transition:all .5s 0s;}
+
+	.about-txt .para p{transform:translateX(-110%);-webkit-transform:translateX(-110%);-moz-transform:translateX(-110%);-ms-transform:translateX(-110%);transition:all .5s 0s;-webkit-transition:all .5s 0s;}
+
+	.about-txt .para{margin-bottom: 50px;}
+
+	.about-txt .para:last-child{margin:120px 0 0 0;}
+	.about-txt p{font-size: 14px;line-height: 2;}
+
+	.about-txt .para:nth-child(1) h3{transition:all .3s 0s;-webkit-transition:all .3s 0s;}
+	.about-txt .para:nth-child(1) p {transition:all .3s .3s;-webkit-transition:all .3s .4s;}
+	.about-txt .para:nth-child(2) p {transition:all .3s .9s;-webkit-transition:all .3s .9s;}
+	.about-txt .para:nth-child(3) p {transition:all .3s 1.2s;-webkit-transition:all .3s 1.2s;}
+	.about-txt .para:nth-child(2) h3{transition:all .3s .6s;-webkit-transition:all .3s .6s;}
+
+
+	.about-img:hover .about-txt h3{transform:translateX(0);-webkit-transform:translateX(0);}
+	.about-img:hover .about-txt .p1 p{transform:translateX(0);-webkit-transform:translateX(0);}
+	.about-img:hover .about-txt .p2 p{transform:translateX(0);-webkit-transform:translateX(0);}
+	.about-img:hover .about-txt .p3 p{transform:translateX(0);-webkit-transform:translateX(0);}
+
+
+	.news-wrap{font-size: 14px;color:#999;box-sizing: border-box;padding-left: 10%;}
+	.news-wrap ul li:first-child{font-size: 16px;margin:15px 0;overflow: visible;margin-bottom: 20px;}
+	.news-wrap:first-child{padding-left: 0;}
+	.news{margin-top: 30px;margin-bottom: 60px;}
+	.news-wrap ul li{cursor: pointer;position: relative;white-space: nowrap;overflow: hidden;text-overflow:ellipsis;line-height: 1.5;}
+	.news-wrap ul li:hover{color:#333;}
+
+	.news-wrap ul li:first-child:after{content:'';display: block;width:32px;height: 1px;background: #999;position: absolute;left:0;bottom: -10px;}
+
+	.news-wrap ul{position: relative}
+	.news-wrap ul:after{content:'';content:'';display: block;width:16px;height: 1px;background: #999;position: absolute;left:0;bottom: -12px;}
+
 
 	@media screen and (max-device-width:768px){
 		.case-wrap>div{width: 50%}
+		.case-wrap>div:last-child{display: none;}
+		.about-img{width:100%;margin-top:5px;padding-left: 0;height: 160px;}
+		.about-img img{height:auto;}
+		.team{padding: 2.5rem 0;}
+		.team-title{font-size: 22px;}
+		.news{display: none;}
 	}
 	@media screen and (max-width:768px){
 		.case-wrap>div{width: 50%}
+		.case-wrap>div:last-child{display: none;}
+		.about-img{width:100%;margin-top:5px;padding-left: 0;height: 160px;}
+		.about-img img{height:auto;}
+		.team{padding: 2.5rem 0;box-sizing: border-box;height: 160px;}
+		.team-title{font-size: 22px;}
+		.news{display: none;}
 	}
 </style>
 
@@ -147,6 +242,27 @@
             		{img:Case13,link:'',name:'紫罗兰'},
             		{img:Case14,link:'',name:'灵丹瑜伽'},
             		{img:'',link:'',name:'更多案例>>'},
+            	],
+            	newsList1:[
+            		{title:'丁子动态 >',link:''},
+            		{title:'LOGO设计公司价格差距是哪些因素造成的',link:''},
+            		{title:'LOGO设计公司怎样提炼设计内容',link:''},
+            		{title:'LOGO设计公司应遵循哪三大原则',link:''},
+            		{title:'影响LOGO设计公司创意设计因素有哪些',link:''},
+            	],
+            	newsList2:[
+            		{title:'丁子观点 >',link:''},
+            		{title:'将品牌形象钉在消费者心智里',link:''},
+            		{title:'品牌快速赢得消费者信任的29种方法',link:''},
+            		{title:'十分钟看懂 聚焦',link:''},
+            		{title:'丁子徐浅谈“钉视觉”美学',link:''},
+            	],
+            	newsList3:[
+            		{title:'行业咨讯 >',link:''},
+            		{title:'微信时代的产品包装设计需将升级',link:''},
+            		{title:'原研哉公开2020年东京奥运会LOGO',link:''},
+            		{title:'美国时装品牌Calvin Klein更换新LOGO',link:''},
+            		{title:'小米公司松果电子品牌LOGO',link:''},
             	]
 
 			}
@@ -160,6 +276,9 @@
 			moreCase(){
 
 			},
+			joinDinz(){
+
+			}
 		},
 		components:{ 
 			vBanner
